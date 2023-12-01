9dffcf71ea60828fce2f2bf4a14486f60c136e6e
HamaHs <lusuy1995@gmail.com>
2023-12-01 16:24:22 +0100

    added DB

---
+++ src/js/DB.js
@@ -0,0 +1,34 @@
+export const listItemsDB = {
+   hero: ["Free Quotes", "100% Commitment-Free"],
+   homeCare: [
+      "Repair and Installation",
+      "Plumbing",
+      "Maintenance",
+      "Budget-friendly",
+      "Home Security Services",
+      "Eco-friendly solutions",
+   ],
+};
+
+export const hightlights = [
+   {
+      img: "../images/hero/Ic_24-Guarantee.svg",
+      altImg: "an image sign of the guarantee",
+      txt: "Satisfaction Guarantee",
+   },
+   {
+      img: "./images/hero/Ic_24-Clock.svg",
+      altImg: "an image sign of a clock",
+      txt: "24H Availability",
+   },
+   {
+      img: "./images/hero/Ic_24-Map-pin.svg",
+      altImg: "an image sign of a map point",
+      txt: "Local US Professional",
+   },
+   {
+      img: "./images/hero/Ic_24-Calendar.svg",
+      altImg: "an image sign of a calendar",
+      txt: "Flexible Appointments",
+   },
+];
